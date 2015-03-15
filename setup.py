bdfce9a7b3c1277e129e51f761eb5baa05d4bcf6
Scott Carver <scott@artificia.org>
2015-03-14 21:27:30 -0700

    better setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,16 @@
-from distutils.core import setup
-setup(name='qpm-test',
-      version='0.1',
-      py_modules=['qpmlib']
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "qpmlib",
+    version = "0.0.1",
+    author = "Scott Carver",
+    author_email = "scott@artificia.org",
+    description = ("Command line utilities for SuperCollider"),
+    license = "BSD",
+    url = "https://github.com/scztt/qpm/tree/qpm-unit",
+    packages=['qpmlib', 'cli'],
 )
