6c50e633899eef13aace610e39efed6050bbbbca
Scott Carver <scott@artificia.org>
2015-03-22 16:30:31 -0700

    refactor to proper python project

---
+++ qpm/core/__init__.py
@@ -0,0 +1,23 @@
+"""qpm core exceptions module."""
+
+class qpmError(Exception):
+    """Generic errors."""
+    def __init__(self, msg):
+        Exception.__init__(self)
+        self.msg = msg
+    
+    def __str__(self):
+        return self.msg
+
+            
+class qpmConfigError(qpmError):
+    pass
+
+class qpmRuntimeError(qpmError):
+    pass
+        
+class qpmArgumentError(qpmError):
+    pass
+
+class qpmInterfaceError(qpmError):
+    pass
